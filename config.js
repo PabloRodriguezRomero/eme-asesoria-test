06c29060c0d4eaa804c8cd977cfff56d518d4a31
Pablo <88206475+PabloRodriguezRomero@users.noreply.github.com>
2023-04-14 12:29:18 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_y6Ospclt8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3f8v1cb6endmvmn3p10sj5e9c4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
